e92f7fe6f5db2365544be265958788e6f2359854
Eric Moret <pro@eric-moret.fr>
2025-11-05 12:00:17 +0100

    1.2 + 1.3

--- main.cpp
+++ main.cpp
@@ -1,7 +1,38 @@
 #include <iostream>
+#include <windows.h> // For console settings
+
+void InitConsole()
+{
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    DWORD mode;
+    GetConsoleMode(hConsole, &mode);
+    SetConsoleMode(hConsole, mode | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
+}
+
+void ClearConsole()
+{
+    std::cout << "\x1b[2J"; // Remove all characters in console
+    std::cout << "\x1b[H"; // Set cursor pos to "home" position (0,0)
+}
+
+void SetCursorVisible(bool visible)
+{
+    if(visible)
+    {
+        std::cout << "\x1b[?25h"; // Make cursor visible
+    }
+    else
+    {
+        std::cout << "\x1b[?25l"; // Make cursor invisible
+    }
+}
 
 int main(int argc, char** argv)
 {
+    InitConsole();
+    ClearConsole();
+    SetCursorVisible(false);
     std::cout << "Hello World" << std::endl;
+    SetCursorVisible(true);
     return 0;
 }
