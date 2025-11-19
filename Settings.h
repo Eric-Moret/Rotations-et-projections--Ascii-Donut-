f5c427d3b9bee30e8771de1a48054394a1d9ae60
Eric-Moret <156897929+Eric-Moret@users.noreply.github.com>
2025-11-19 16:33:52 +0100

    2.4

--- Settings.h
+++ Settings.h
@@ -6,7 +6,11 @@ class Settings
     Settings(int argc, char** argv);
     int GetScreenWidth() const { return m_screenWidth; }
     int GetScreenHeight() const { return m_screenHeight; }
+    char GetScreenBackground() const { return m_screenBackground; }
+    float GetScreenPosition() const { return m_screenPosition; }
+    char GetScreenMeshProjection() const { return m_screenMeshProjection; }
     int GetMeshResolution() const { return m_meshResolution; }
+    float GetMeshPosition() const { return m_meshPosition; }
 
     private:
     void _ParseArguments(int argc, char** argv);
@@ -14,5 +18,9 @@ class Settings
     private:
     int m_screenWidth;
     int m_screenHeight;
+    char m_screenBackground;
+    float m_screenPosition;
+    char m_screenMeshProjection;
     int m_meshResolution;
+    float m_meshPosition;
 };
